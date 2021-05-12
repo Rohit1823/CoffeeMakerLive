d94a4a4a20fff8c8a947a76044ccb17b4550cdcf
Rohit1823 <rohitmachale1823@gmail.com>
2021-05-13 00:44:18 +0530

    [Rohit]ADD: Coffee Maker Live Problem

---
+++ CoffeeMakerLive/CoffeeMakerLive/Starbucks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeMakerLive
+{
+    public class StubStarbucks
+    {
+        public class Starbucks : IMakeACoffee
+        {
+            public bool CheckIngredientAvailability()
+            {
+                return true;
+            }
+
+            public string CoffeeMaking(int sugerPerSpoon, int CoffeePack)
+            {
+                return "";
+            }
+        }
+
+    }
+}
